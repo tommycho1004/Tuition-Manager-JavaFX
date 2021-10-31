908770c2a9c61aaf336c0e61d0a5ab5ce1399a4c
Dong Gun (Tommy) Cho <tommycho1004@gmail.com>
2021-10-31 16:08:40 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -3,20 +3,53 @@ package com.example.smproject3;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+
+import java.time.format.DateTimeFormatter;
 
 public class HelloController {
 
-    private Roster roster;
+    private Roster roster = new Roster();
 
     int minCredits = 3;
     int maxCredits = 24;
     int zero = 0;
 
     @FXML
+    private ToggleGroup Major;
+
+    @FXML
     private Button addButton;
 
     @FXML
+    private RadioButton baButton;
+
+    @FXML
+    private RadioButton baButton2;
+
+    @FXML
+    private Button calculateButton;
+
+    @FXML
     private TextField creditHrs;
+
+    @FXML
+    private RadioButton csButton;
+
+    @FXML
+    private RadioButton csButton2;
+
+    @FXML
+    private RadioButton ctButton;
+
+    @FXML
+    private RadioButton eeButton;
+
+    @FXML
+    private RadioButton eeButton2;
+
+    @FXML
+    private TextField finAidAmountEnter;
 
     @FXML
     private ToggleGroup ifResident;
@@ -25,13 +58,25 @@ public class HelloController {
     private RadioButton internationalButton;
 
     @FXML
+    private RadioButton itButton;
+
+    @FXML
+    private RadioButton itButton2;
+
+    @FXML
     private ToggleGroup majors;
 
     @FXML
     private RadioButton meButton;
 
     @FXML
+    private RadioButton meButton2;
+
+    @FXML
     private TextField nameEnter;
+
+    @FXML
+    private TextField nameEnter2;
 
     @FXML
     private ToggleGroup nonResType;
@@ -43,16 +88,34 @@ public class HelloController {
     private RadioButton nyButton;
 
     @FXML
-    private RadioButton ctButton;
+    private TextArea output;
 
     @FXML
-    private TextArea output;
+    private Button payButton;
+
+    @FXML
+    private DatePicker paymentDateEnter;
+
+    @FXML
+    private TextField paymentEnter;
+
+    @FXML
+    private Button printButton;
+
+    @FXML
+    private Button printByNameButton;
+
+    @FXML
+    private Button printByPaymentButton;
 
     @FXML
     private Button removeButton;
 
     @FXML
     private RadioButton residentButton;
+
+    @FXML
+    private Button setFinAidButton;
 
     @FXML
     private CheckBox studyAbroadButton;
@@ -68,6 +131,8 @@ public class HelloController {
 
     @FXML
     private Button tuitionDueButton;
+
+
 
     @FXML
     boolean creditChecker() {
@@ -91,7 +156,6 @@ public class HelloController {
         } else return true;
     }
 
-    @FXML
     public boolean dataChecker() {
         String stuName = nameEnter.getText();
         if (stuName.isEmpty()) {
@@ -190,5 +254,130 @@ public class HelloController {
 
     }
 
-}
+    //2nd Tab Methods Here
+    @FXML
+    void financialAidEnter(KeyEvent event) {
+        setFinAidButton.setDisable(false);
+        payButton.setDisable(true);
+        paymentEnter.clear();
+        paymentDateEnter.setValue(null);
+    }
 
+    @FXML
+    void paymentAmountEnter(KeyEvent event) {
+        payButton.setDisable(false);
+        setFinAidButton.setDisable(true);
+        finAidAmountEnter.clear();
+    }
+
+    @FXML
+    public boolean dataChecker2() {
+        String stuName = nameEnter2.getText();
+        if (stuName.isEmpty()) {
+            output.appendText("Name data missing.\n");
+            return false;
+        }
+        RadioButton major = (RadioButton) Major.getSelectedToggle();
+        if (major == null) {
+            output.appendText("Major data missing.\n");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean dateChecker(){
+        if (paymentDateEnter.getValue() == null){
+            output.appendText("Date missing.\n");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public boolean financialAidChecker(double financialAid) {
+        if (financialAid < 0 || financialAid > 10000) {
+            output.appendText("Invalid amount.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @FXML
+    void setFinAid(ActionEvent event) {
+        String name = nameEnter2.getText();
+        RadioButton major2 = (RadioButton) Major.getSelectedToggle();
+        String major = major2.getText();
+        double finAidAmount = Double.parseDouble(finAidAmountEnter.getText());
+        if (financialAidChecker(finAidAmount)) {
+            Student temp = new Student(name, major);
+            int aidScenario = roster.setFinancialAid(temp, finAidAmount);
+            if (aidScenario == 4) {
+                output.appendText("Tuition updated.");
+            } else if (aidScenario == 0) {
+                output.appendText("Student not in the roster.");
+            } else if (aidScenario == 1) {
+                output.appendText("Not a resident student.");
+            } else if (aidScenario == 2) {
+                output.appendText("Awarded once already.");
+            } else if (aidScenario == 3) {
+                output.appendText("Parttime student doesn't qualify for the award.");
+            }
+        }
+    }
+
+    @FXML
+    void payTuition(ActionEvent event) {
+        if (dataChecker2()) {
+            String name = nameEnter2.getText();
+            RadioButton major2 = (RadioButton) Major.getSelectedToggle();
+            String major = major2.getText();
+            double paymentAmount = Double.parseDouble(paymentEnter.getText());
+            if (dateChecker()){
+                Date date = new Date(paymentDateEnter.getValue().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+                if (!date.isValid()) {
+                    output.appendText("Payment date invalid.\n");
+                }
+                else {
+                    if (paymentAmount > 0.0) {
+                        Student student = new Student(name, major);
+                        if (roster.find(student) == -1){
+                            output.appendText("Student not found.\n");
+                        }
+                        else if (!roster.payTuition(student, paymentAmount, date)) {
+                            output.appendText("Amount is greater than the amount due.\n");
+                        } else {
+                            output.appendText("Payment applied.\n");
+                        }
+                    }
+                    else{
+                        output.appendText("Payment amount invalid.\n");
+                    }
+                }
+            }
+        }
+    }
+
+    //for third tab
+
+    @FXML
+    void calculateTuition(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printRoster(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printRosterByName(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printRosterByPayment(ActionEvent event) {
+
+    }
+}
